f8b911836d7a09084ccd71885178a05eeea9649e
Joshua Comeau <joshwcomeau@gmail.com>
2015-02-21 20:11:14 -0500

    Imported jQuery, organized scripts in a js folder. Added a content script that fires on yelp domains, and made the page action show up on yelp pages.

---
+++ extension/js/show_icon.js
@@ -0,0 +1,16 @@
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([{
+      // That fires when a page's URL contains 'yelp' ...
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { urlContains: 'yelp' },
+        })
+      ],
+      // And shows the extension's page action.
+      actions: [ new chrome.declarativeContent.ShowPageAction() ]
+    }]);
+  });
+});
