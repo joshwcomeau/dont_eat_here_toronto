c8a45bd028ed95a25c717811aeb944a6bc5eb64d
Joshua Comeau <joshwcomeau@gmail.com>
2015-02-21 22:27:28 -0500

    Updated manifest to allow for access to local json files. Wrote a function to tally the status counts for a given restaurant.

--- extension/js/get_and_display_results.js
+++ extension/js/get_and_display_results.js
@@ -1,3 +1,19 @@
+function tallyStatuses(data) {
+  var tally, status, report;
+
+  tally = {
+    pass: 0,
+    conditional: 0,
+    closed: 0
+  };
+
+  data.inspections.forEach(function(report) {
+    tally[report.status]++;
+  });
+
+  return tally;
+}
+
 
 // Build our DOM element
 var inspectorDOM = $("<div>").addClass("inspection-wrapper").append("<h4>1 Minor Infraction, 2 Significant Infractions</h4>")
@@ -5,3 +21,16 @@ var inspectorDOM = $("<div>").addClass("inspection-wrapper").append("<h4>1 Minor
 
 // Insert our DOM element
 $(".biz-page-title").after(inspectorDOM);
+
+
+// Get the data
+$.get(chrome.extension.getURL("fixture.json"), function(data) {
+  data = JSON.parse(data);
+  console.log(data);
+
+  // Get the second restaurant's statuses
+  statuses = tallyStatuses(data[1]);
+
+  console.log(statuses);
+  $(".inspection-wrapper").html(statuses);
+})
