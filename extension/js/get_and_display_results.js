5817569e9f055c36d99b465f938f9ce0d988f221
Joshua Comeau <joshwcomeau@gmail.com>
2015-02-21 20:43:54 -0500

    Added a proper dev setup with NPM and gulp. Added a variables.scss file.

--- extension/js/get_and_display_results.js
+++ extension/js/get_and_display_results.js
@@ -1,1 +1,7 @@
-alert("hi!");
+
+// Build our DOM element
+var inspectorDOM = $("<div>").addClass("inspection-wrapper").append("<h4>1 Minor Infraction, 2 Significant Infractions</h4>")
+
+
+// Insert our DOM element
+$(".biz-page-title").after(inspectorDOM);
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss    = require('gulp-minify-css'),
+    jshint       = require('gulp-jshint'),
+    uglify       = require('gulp-uglify'),
+    rename       = require('gulp-rename'),
+    concat       = require('gulp-concat'),
+    notify       = require('gulp-notify'),
+    livereload   = require('gulp-livereload'),
+    sourcemaps   = require('gulp-sourcemaps');
+
+
+gulp.task('default', function() {
+  gulp.start('styles', 'scripts');
+});
+
+gulp.task('styles', function() {
+  return gulp.src('extension/sass/**/*.scss')  
+    .pipe(sass())  
+    .pipe(concat('style.css')) 
+    .pipe(gulp.dest('extension/css/')) 
+    .pipe(notify({ message: 'Styles task complete'}));
+});
+
+gulp.task('scripts', function() {
+ 
+});
